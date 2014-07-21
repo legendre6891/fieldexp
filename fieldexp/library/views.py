d74fb9f1cd1d9cc43317a072754309caaf95a498
Kevin Li <kli6891@gmail.com>
2014-07-21 14:54:41 -0500

    started working on views

--- fieldexp/library/views.py
+++ fieldexp/library/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, render_to_response, RequestContext
+from django.core.urlresolvers import reverse
 # Create your views here.
+
+def index(request):
+    return render_to_response('library/index.html', locals(),
+            context_instance=RequestContext(request))
