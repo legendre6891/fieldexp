5765aadd3c0476fce43218f30291c659af8f3d2d
mepps32 <mepps32@gmail.com>
2014-07-31 16:04:08 -0500

    Adding Heroku Necessities

---
+++ fieldexp/Procfile
@@ -0,0 +1,1 @@
+web: gunicorn fieldexp.wsgi --log-file -
--- fieldexp/fieldexp/settings.py
+++ fieldexp/fieldexp/settings.py
@@ -8,6 +8,16 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header the request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
@@ -81,5 +91,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
+
+STATICFILES_DIRS = (
+                    os.path.join(BASE_DIR, 'static'),
+)
+
+# Media files
 MEDIA_ROOT = '/home/myusername/mypaper/' ## Change this
 MEDIA_URL = '/media/'
